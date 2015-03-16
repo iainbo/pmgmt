c22bfacb2af5c6d1f7b1a0917b0f9be5440dc544
iainbo <iainbo@users.noreply.github.com>
2015-03-16 20:34:05 +0000

    Member class updated to allow MySQL sequence to be called. PrimeFaces added and theme applied.

--- pmgmt-ejb/src/main/java/org/iainbo/model/Member.java
+++ pmgmt-ejb/src/main/java/org/iainbo/model/Member.java
@@ -18,12 +18,7 @@ package org.iainbo.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -40,7 +35,7 @@ public class Member implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
