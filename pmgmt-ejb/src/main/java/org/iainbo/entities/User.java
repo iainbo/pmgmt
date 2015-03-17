3d5d09cebaa75b68d5eb0fb6f0a930d1cb771af8
iainbo <iainbo@users.noreply.github.com>
2015-03-17 19:24:58 +0000

    Basic User Class created and persistence.xml updated to create database tables. import.sql adds an entry to members table to avoid error on launch.

---
+++ pmgmt-ejb/src/main/java/org/iainbo/entities/User.java
@@ -0,0 +1,10 @@
+package org.iainbo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Users")
+public class User implements Serializable{
+}
