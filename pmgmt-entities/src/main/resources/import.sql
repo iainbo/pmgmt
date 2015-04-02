40b9236fd065f53d23952d86ede20bf3ac3d75b6
iainbo <iainbo@users.noreply.github.com>
2015-04-02 16:25:35 +0100

    Application configured to use JAAS for logging in.

--- pmgmt-entities/src/main/resources/import.sql
+++ pmgmt-entities/src/main/resources/import.sql
@@ -1,8 +1,8 @@
 -- This file us used to load seed data into the database using SQL statements on Application build
 
-INSERT INTO USERS(user_name, password, first_name, surname, email, phone_number, date_created) VALUES('admin', 'password', 'Admin', 'User', 'test@email.com', '123456789', sysdate());
+INSERT INTO USERS(user_name, password, first_name, surname, email, phone_number, date_created) VALUES('admin', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 'Admin', 'User', 'test@email.com', '123456789', sysdate());
 
-INSERT INTO USERS(user_name, password, first_name, surname, email, phone_number, date_created) VALUES('test', 'password', 'Admin', 'User', 'test@email.com', '123456789', sysdate());
+INSERT INTO USERS(user_name, password, first_name, surname, email, phone_number, date_created) VALUES('test', 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=', 'Admin', 'User', 'test@email.com', '123456789', sysdate());
 
 INSERT INTO ROLES(DATE_CREATED, ROLE) VALUES (sysdate(), 'ADMIN');
 
@@ -11,4 +11,3 @@ INSERT INTO ROLES(DATE_CREATED, ROLE) VALUES (sysdate(), 'USER');
 INSERT INTO USER_ROLES(ROLE_ID, USER_ID) VALUES ((SELECT ID FROM ROLES WHERE ROLE = 'ADMIN'), (SELECT ID FROM USERS WHERE USER_NAME = 'admin'));
 
 INSERT INTO USER_ROLES(ROLE_ID, USER_ID) VALUES ((SELECT ID FROM ROLES WHERE ROLE = 'USER'), (SELECT ID FROM USERS WHERE USER_NAME = 'test'));
-
