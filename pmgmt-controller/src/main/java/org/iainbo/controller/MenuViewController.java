76c5b079a47470d10ae965ab2ac0da48619731b1
iainbo <iainbo@users.noreply.github.com>
2015-04-24 20:06:38 +0100

    A user can logout

--- pmgmt-controller/src/main/java/org/iainbo/controller/MenuViewController.java
+++ pmgmt-controller/src/main/java/org/iainbo/controller/MenuViewController.java
@@ -3,6 +3,7 @@ package org.iainbo.controller;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import java.io.IOException;
 
 @ManagedBean
 
@@ -23,5 +24,14 @@ public class MenuViewController {
     public void addMessage(String summary, String detail) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
         FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void logout(){
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/pmgmt/");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
