34e31ef29f91f1d768859116a7148c164d4f3289
iainbo <iainbo@users.noreply.github.com>
2015-05-29 07:55:59 +0100

    Images within a gallery are displayed in a datagrid

--- pmgmt-services/target/generated-sources/org/iainbo/pmgmt/service/mapper/GalleryMapperImpl.java
+++ pmgmt-services/target/generated-sources/org/iainbo/pmgmt/service/mapper/GalleryMapperImpl.java
@@ -15,7 +15,7 @@ import org.iainbo.entities.user.User;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2015-05-28T15:07:55+0100"
+    date = "2015-05-29T07:54:34+0100"
 )
 @ApplicationScoped
 public class GalleryMapperImpl implements GalleryMapper {
