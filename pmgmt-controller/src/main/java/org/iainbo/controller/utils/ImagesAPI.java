b22f72df9ca05103be0252835d7e923ee7113f10
iainbo <iainbo@users.noreply.github.com>
2015-05-29 20:08:06 +0100

    When a thumbnail or image is not available, a no photo available image is displayed

--- pmgmt-controller/src/main/java/org/iainbo/controller/utils/ImagesAPI.java
+++ pmgmt-controller/src/main/java/org/iainbo/controller/utils/ImagesAPI.java
@@ -1,5 +1,6 @@
 package org.iainbo.controller.utils;
 
+import org.apache.commons.io.IOUtils;
 import org.iainbo.pmgmt.service.images.GalleryService;
 import org.iainbo.pmgmt.service.images.ImageService;
 
@@ -15,6 +16,7 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Named(value = "imagesAPI")
 @Stateless
@@ -35,8 +37,13 @@ public class ImagesAPI{
 
         byte[] imageBytes = imageService.getBytesForImage(image);
         if(imageBytes == null || imageBytes.length == 0){
-            //TODO: create no image available image.
-            System.out.println("No Image Available for this ID: " + imageId);
+            InputStream noImageAvailable = ImagesAPI.class.getResourceAsStream("/img/photo_not_available.jpg");
+            try {
+
+                imageBytes = IOUtils.toByteArray(noImageAvailable);
+            } catch (NullPointerException npe) {
+                System.out.println("No Image Available for this ID: " + imageId);
+            }
         }
         Response.ResponseBuilder imageResponseBuilder = Response.ok(new ByteArrayInputStream(imageBytes), "image/jpeg");
         imageResponseBuilder.header("Content-Disposition", "attachment;filename=\"image.jpeg\"");
@@ -50,8 +57,13 @@ public class ImagesAPI{
         Long gallery = Long.valueOf(galleryId);
         byte[] thumbBytes = galleryService.getThumbnailForGallery(gallery);
         if(thumbBytes == null || thumbBytes.length == 0){
-            //TODO: create no image available image.
-            System.out.println("No thumbnail available for: " + galleryId);
+            InputStream noImageAvailable = ImagesAPI.class.getResourceAsStream("/img/photo_not_available.jpg");
+            try {
+
+                thumbBytes = IOUtils.toByteArray(noImageAvailable);
+            } catch (NullPointerException npe) {
+                System.out.println("No thumbnail available for: " + galleryId);
+            }
         }
         Response.ResponseBuilder imageResponseBuilder = Response.ok(new ByteArrayInputStream(thumbBytes), "image/jpeg");
         imageResponseBuilder.header("Content-Disposition", "attachment;filename=\"image.jpeg\"");
--- pmgmt-services/target/generated-sources/org/iainbo/pmgmt/service/mapper/UserMapperImpl.java
+++ pmgmt-services/target/generated-sources/org/iainbo/pmgmt/service/mapper/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.iainbo.entities.user.User;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2015-05-29T09:33:47+0100"
+    date = "2015-05-29T20:06:34+0100"
 )
 @ApplicationScoped
 public class UserMapperImpl implements UserMapper {
