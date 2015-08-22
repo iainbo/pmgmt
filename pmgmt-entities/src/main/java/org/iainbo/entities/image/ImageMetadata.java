ddad5c638addb297aea4f880d0443819b797ea88
iainbo <iainbo@users.noreply.github.com>
2015-08-22 11:48:33 +0100

    The image metadata class has been removed

--- pmgmt-entities/src/main/java/org/iainbo/entities/image/ImageMetadata.java
+++
@@ -1,4 +0,0 @@
-package org.iainbo.entities.image;
-
-public class ImageMetadata {
-}
--- pmgmt-services/target/generated-sources/org/iainbo/pmgmt/service/mapper/GalleryMapperImpl.java
+++ pmgmt-services/target/generated-sources/org/iainbo/pmgmt/service/mapper/GalleryMapperImpl.java
@@ -15,7 +15,7 @@ import org.iainbo.entities.user.User;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2015-08-22T11:37:27+0100"
+    date = "2015-08-22T11:46:54+0100"
 )
 @ApplicationScoped
 public class GalleryMapperImpl extends GalleryMapper {
